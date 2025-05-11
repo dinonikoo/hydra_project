48341d4c17d0c5bb15b81646a5c73b4bafd66313
dinonikoo <145788052+dinonikoo@users.noreply.github.com>
2025-05-11 13:35:33 +0300

    generated Lists.java from Lists.hs (declaration of a specific value)

---
+++ samples/gen/Lists.java
@@ -0,0 +1,16 @@
+// Note: this is an automatically generated file. Do not edit.
+
+/**
+ * A module with lists
+ */
+public interface Lists {
+  java.util.List<Integer> exampleList = java.util.Arrays.asList(
+    1,
+    2,
+    3);
+  
+  java.util.List<String> stringList = java.util.Arrays.asList(
+    "hello",
+    "hydra",
+    "dsl");
+}
