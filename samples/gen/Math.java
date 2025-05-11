db4431e0049a8fccd8ae054e753ff2db316ea017
dinonikoo <145788052+dinonikoo@users.noreply.github.com>
2025-05-11 13:36:59 +0300

    generated Math.java from MyMath.hs (basic functions example)

---
+++ samples/gen/Math.java
@@ -0,0 +1,46 @@
+// Note: this is an automatically generated file. Do not edit.
+
+/**
+ * Basic math functions
+ */
+public interface Math {
+  static Integer neg(Integer x) {
+    return hydra.lib.math.Neg.apply((x));
+  }
+  
+  static Integer add(Integer x) {
+    return hydra.lib.math.Add.apply(
+      (x),
+      1);
+  }
+  
+  static Integer sub(Integer x) {
+    return hydra.lib.math.Sub.apply(
+      (x),
+      1);
+  }
+  
+  static Integer mul(Integer x) {
+    return hydra.lib.math.Mul.apply(
+      (x),
+      1);
+  }
+  
+  static Integer div(Integer x) {
+    return hydra.lib.math.Div.apply(
+      (x),
+      1);
+  }
+  
+  static Integer mod(Integer x) {
+    return hydra.lib.math.Mod.apply(
+      (x),
+      1);
+  }
+  
+  static Integer rem(Integer x) {
+    return hydra.lib.math.Rem.apply(
+      (x),
+      1);
+  }
+}
