abff52ec67858751739bc2ded593b4ebc89eef01
dinonikoo <145788052+dinonikoo@users.noreply.github.com>
2025-05-11 13:31:55 +0300

    generated from Chars.hs

---
+++ samples/gen/Chars.java
@@ -0,0 +1,22 @@
+// Note: this is an automatically generated file. Do not edit.
+
+/**
+ * Char utilities
+ */
+public interface Chars {
+  static Boolean isLower(Integer c) {
+    return hydra.lib.chars.IsLower.apply((c));
+  }
+  
+  static Boolean isUpper(Integer c) {
+    return hydra.lib.chars.IsUpper.apply((c));
+  }
+  
+  static Integer toLower(Integer c) {
+    return hydra.lib.chars.ToLower.apply((c));
+  }
+  
+  static Integer toUpper(Integer c) {
+    return hydra.lib.chars.ToUpper.apply((c));
+  }
+}
